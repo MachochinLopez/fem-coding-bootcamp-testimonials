34a074cc60525689464ec340f9ac15e15040c21c
Oscar López <machochinnio@hotmail.com>
2021-02-02 16:54:38 -0600

    Animations and design only for mobile target

--- src/js/change_testimonial.js
+++ src/js/change_testimonial.js
@@ -3,25 +3,51 @@ const buttonPrevious = document.querySelector(".testimonials__button--previous")
 const buttonNext = document.querySelector(".testimonials__button--next");
 let active = document.querySelector(".testimonials__item--active");
 
+// Cambia al testimonial anterior.
 buttonPrevious.addEventListener("click", function() {
-  const previousElement = active.previousElementSibling;
+  const previousElement = 
+        active.previousElementSibling !== null 
+          ? active.previousElementSibling 
+          : testimonials[testimonials.length - 1];
 
-  if(previousElement !== null) {
-    changeActiveElement(active, previousElement);
-  }
+  changeActiveElement(active, previousElement, false);
 });
 
+// Cambia al testimonial siguiente.
 buttonNext.addEventListener("click", function() {
-  const nextElement = active.nextElementSibling;
-  console.log(nextElement);
-  
-  if(nextElement !== null) {
-    changeActiveElement(active, nextElement);
-  }
+  const nextElement = 
+        active.nextElementSibling !== null 
+          ? active.nextElementSibling 
+          : testimonials[0];
+
+  changeActiveElement(active, nextElement, true);
 });
 
 // Le quita la clase activa al elemento activo actual y se la agrega al nuevo elemento.
-function changeActiveElement(previousActiveElement, nextActiveElement) {
+// previousActiveElement: Elemento que estaba activo.
+// nextActiveElement:     Elemento que será el testimonio activo.
+// isNext:                Dirección a la que va a girar el container. true=next, false=previous.
+function changeActiveElement(previousActiveElement, nextActiveElement, isNext) {
+  // Remueve todas las clases de animación.
+  const allItems = document.querySelectorAll(".testimonials__item");
+
+  for (const item of allItems) {
+    item.classList.remove("testimonials__item--exit-left");
+    item.classList.remove("testimonials__item--exit-right");
+    item.classList.remove("testimonials__item--enter-left");
+    item.classList.remove("testimonials__item--enter-right");
+  }
+
+  // Agrega las nuevas clases de animación.
+  if(isNext) {
+    previousActiveElement.classList.add("testimonials__item--exit-right");
+    nextActiveElement.classList.add("testimonials__item--enter-left");
+  } else {
+    previousActiveElement.classList.add("testimonials__item--exit-left");
+    nextActiveElement.classList.add("testimonials__item--enter-right");
+  }
+
+  // Cambia cuál es el elemento activo.
   previousActiveElement.classList.remove("testimonials__item--active");
   nextActiveElement.classList.add("testimonials__item--active");
   active = nextActiveElement;
